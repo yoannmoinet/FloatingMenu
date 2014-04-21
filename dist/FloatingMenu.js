97cd0095dd139275d59be3bb1f12d1003d17448f
Yoann Moinet <yoann.moinet@gmail.com>
2014-04-20 22:44:35 -0400

    Initial Commit

---
+++ dist/FloatingMenu.js
@@ -0,0 +1,23 @@
+(function(root, undefined) {
+
+  "use strict";
+
+
+/* FloatingMenu main */
+
+// Base function.
+var FloatingMenu = function() {
+  // Add functionality here.
+  return true;
+};
+
+
+// Version.
+FloatingMenu.VERSION = '0.0.0';
+
+
+// Export to the root, which is probably `window`.
+root.FloatingMenu = FloatingMenu;
+
+
+}(this));
---
+++ src/main.js
@@ -0,0 +1,15 @@
+/* FloatingMenu main */
+
+// Base function.
+var FloatingMenu = function() {
+  // Add functionality here.
+  return true;
+};
+
+
+// Version.
+FloatingMenu.VERSION = '0.0.0';
+
+
+// Export to the root, which is probably `window`.
+root.FloatingMenu = FloatingMenu;
